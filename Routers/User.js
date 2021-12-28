4eda4d19bd47d88380ea39f60949ab2abf85c2af
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-12-28 05:36:39 +0300

    Routers

--- Routers/User.js
+++ Routers/User.js
@@ -3,11 +3,72 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/user", async (req, res) => {
-    
+  try {
+    const allusers = await UserModel.find();
+    res.send({
+      status: 200,
+      message: "Successfull",
+      Users: allusers,
+    });
+  } catch (error) {
+    res.send({
+      status: 404,
+      message: `Error: ${error}`,
+    });
+  }
 });
 
-router.post("/user", async (req, res) => {});
+router.post("/create-User", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  try {
+    const user = new UserModel();
+    const result = await user.save();
+    res.send({
+      status: 200,
+      message: `Successfully created`,
+      User: user,
+    });
+  } catch (error) {
+    res.send({
+      status: 400,
+      message: `Error: ${error}`,
+    });
+  }
+});
 
-router.put("/user", async (req, res) => {});
+router.put("/update-user/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  try {
+    const updateUser = await UserModel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.send({
+      status: 200,
+      message: "Successfully Updated",
+    });
+  } catch (error) {
+    res.send({
+      status: 400,
+      message: `Error: ${error}`,
+    });
+  }
+});
 
-router.delete("/user", async (req, res) => {});
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const deleteUser = await UserModel.findByIdAndDelete(req.params.id);
+    res.send({
+      status: 200,
+      message: "Successfully Deleted",
+    });
+  } catch (error) {
+    res.send({
+      status: 400,
+      message: `Error: ${error}`,
+    });
+  }
+});
