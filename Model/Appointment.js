f58b6a40c85956e1d4a0f73fbd039b9051d53b11
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-12-30 15:10:16 +0300

    Model

---
+++ Model/Appointment.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const appointmentSchema = new mongoose.Schema({
+  // userID DoctorID Time Cabasho
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  doctorId: { type: mongoose.Schema.Types.ObjectId, ref: "DoctorModel" },
+  appointmentTime: { type: Date, default: Date.now() },
+  description: String,
+});
+
+function validate(appointmentSchema) {
+  const appointmentValidation = Joi.object({
+    userId: Joi.string().required(),
+    doctorId: Joi.string().required(),
+    appointmentTime: Joi.string().required(),
+    description: Joi.string().required(),
+  });
+
+  return appointmentValidation.validate(appointmentSchema);
+}
+
+const AppointmentModel = mongoose.model("AppointmentModel", appointmentSchema);
+
+exports.AppointmentModel = AppointmentModel;
+exports.validate = validate;
