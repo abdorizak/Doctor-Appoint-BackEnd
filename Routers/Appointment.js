997e621d571105a5ae7fb994b20f307e26736bdd
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-01-01 15:31:38 +0300

    Routers

--- Routers/Appointment.js
+++ Routers/Appointment.js
@@ -21,3 +21,36 @@ router.get("/appointment", async (req, res) => {
     });
   }
 });
+
+router.post("/make-appointment", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+  try {
+    const makeAppointment = new AppointmentModel();
+    const result = await makeAppointment.save();
+    res.send({
+      status: 200,
+      message: "SuccessFull",
+      Appointment: result,
+    });
+  } catch (error) {
+    res.send({
+      status: 404,
+      message: `Error : ${error}`,
+    });
+  }
+});
+
+router.delete("/delete-Appointment/:id", async (req, res) => {
+  try {
+    const deleteAppointment = await AppointmentModel.findByIdAndDelete(
+      req.params.id
+    );
+    res.send({
+      status: 200,
+      message: "Successfully Deleted",
+    });
+  } catch (error) {
+    
+  }
+});
