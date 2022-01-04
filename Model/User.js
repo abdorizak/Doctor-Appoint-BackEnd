b4ef92dbb1c1a3678ce6b39f71531299f9e04db9
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-01-04 07:39:27 +0300

    Model

--- Model/User.js
+++ Model/User.js
@@ -2,6 +2,7 @@
  * Copyright (c) 2022
  * @ author:  Abdorizak Abdalla aka (Xman)
  */
+const jwt = require("jsonwebtoken");
 const Joi = require("Joi");
 const mongoose = require("mongoose");
 
@@ -13,12 +14,22 @@ const userSchema = new mongoose.Schema({
 
 function validate(userSchema) {
   const userValidation = Joi.object({
-    name: Joi.string().required(),
     username: Joi.string().min(3).required(),
     password: Joi.string().min(5).required(),
   });
   return userValidation.validate(userSchema);
 }
+
+userSchema.methods.generateAuthToken = async () => {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      username: this.name,
+    },
+    "JwtPrivateKey"
+  );
+  return token;
+};
 
 const UserModel = mongoose.model("User", userSchema);
 
