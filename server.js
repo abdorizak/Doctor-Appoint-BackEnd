dfaea8152c729b6fb75b42de5e24fba4a458d2e2
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-01-06 09:36:14 +0300

    Server.js

--- server.js
+++ server.js
@@ -4,20 +4,23 @@
  */
 const connectToMongo = require("./config/db");
 const Auth = require("./Routers/Auth");
-const User = require("./Routers/User");
-const category = require("./Routers/Category");
-const doctors = require("./Routers/Doctor");
+const User = require("./Routers/User.Router");
+const category = require("./Routers/Category.Router");
+const doctors = require("./Routers/Doctor.Router");
 const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
 app.use(express.json());
+app.use(cors());
 
 connectToMongo();
 
-app.use("/api", Auth);
-app.use("/api", User);
-app.use("/api", category);
-app.use("/api", doctors);
+app.use("/api/auth", Auth);
+app.use("/api/user", User);
+app.use("/api/category", category);
+app.use("/api/doctors", doctors);
 
 const port = process.env.PORT || 9000;
 
