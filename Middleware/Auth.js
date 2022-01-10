578f9080021cd6a27b65e012b6c878ac6a047094
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-01-10 22:05:49 +0300

    Middleware

--- Middleware/Auth.js
+++ Middleware/Auth.js
@@ -2,8 +2,6 @@ const jwt = require("jsonwebtoken");
 
 function Auth(req, res, next) {
   const token = req.headers["authorization"];
-  // console.log(req.headers);
-  console.log(token);
   if (!token)
     return res
       .status(401)
@@ -12,15 +10,13 @@ function Auth(req, res, next) {
     const tokn = token.slice(7);
     jwt.verify(tokn, process.env.SECRET_ACCESS_TOKEN, (err, decoded) => {
       if (err) {
-        console.log(err);
-        return res.status(401).send(err);
+        return res.status(401).send({ message: "InvalidToken" });
       } else {
         req.user = decoded;
         next();
       }
     });
   } catch (error) {
-    console.log(error);
     res.status(401).send({ message: `Error: ${error}` });
   }
 }
