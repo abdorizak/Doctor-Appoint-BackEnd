311f5e800e725852aba7ab97b2b51d13cd692f2b
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-01-15 16:39:42 +0300

    Server

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const Auth = require("./Routers/Auth");
 const User = require("./Routers/User.Router");
 const category = require("./Routers/Category.Router");
 const doctors = require("./Routers/Doctor.Router");
+const appointment = require("./Routers/Appointment.Router");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -21,6 +22,7 @@ app.use("/api/auth", Auth);
 app.use("/api/user", User);
 app.use("/api/category", category);
 app.use("/api/doctors", doctors);
+app.use("/api/appointment", appointment);
 
 const port = process.env.PORT || 9000;
 
